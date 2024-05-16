4c04668997bc62ce29f6e54df2ca89511aa9618d
ElisabettaMirti <belmirti@gmail.com>
2024-05-16 17:05:02 +0200

    inserito pseudocodice base

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+//Creo una variabile con prompt in cui chiedo all'utente una parola
+
+//Creo una funzione con ciclo for che metta a confronto le lettere partendo dall'inizio e dalla fine 
