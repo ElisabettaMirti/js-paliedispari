9ce15820284e7053c13900d04e93a15d84a0ae03
ElisabettaMirti <belmirti@gmail.com>
2024-05-16 17:38:17 +0200

    aggiunto pseudocodice per pari e dispari

---
+++ Pari e Dispari/js/script.js
@@ -0,0 +1,18 @@
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+//Creo un prompt che chieda all'utente se pari o dispari
+    //Inserisco una condizione che assegna automaticamente pari se la risposta non è accettabile
+
+//Creo un prompt che chieda all'utente un numero da 1 a 5
+    //Inserisco una condizione che rifiuta quasiasi risposta che non sia un numero da 1 a 5
+
+//Creo una funzione che generi un numero random da 1 a 5 per il computer
+
+//Stampo in console la somma dei due numeri
+
+//Creo una funzione che distingua un numero pari da un numero dispari
+
+//Stampo in console il risultato
